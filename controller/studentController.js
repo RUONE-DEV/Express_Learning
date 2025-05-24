fb45d4dcba5ec614336e17cca8d110dcb4c6ca17
RUONE-DEV <ruwandilhara81@gmail.com>
2025-05-24 23:08:15 +0530

    updated  the Student Controller

--- controller/studentController.js
+++ controller/studentController.js
@@ -1,3 +1,8 @@
-exports.getAllStudents = catchAsync(async (res ,req ,next) => {
-    res
-})
+let items = [
+  { id: 1, name: 'Apple', quantity: 10 },
+  { id: 2, name: 'Banana', quantity: 5 },
+];
+
+exports.getAllStudents =  (req ,res) =>{
+    res.json(items);
+}
